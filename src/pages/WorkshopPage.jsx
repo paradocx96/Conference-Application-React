a83c60447fb544f3f01ffded4a23081f9c925aad
Amarangi Karunathilaka <amarangikarunathilaka@gmail.com>
2021-05-24 21:51:32 +0530

    create workshop common guideline dummy list.

--- src/pages/WorkshopPage.jsx
+++ src/pages/WorkshopPage.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import CreateWorkshop from "../components/workshop/CreateWorkshop";
+import WorkshopCommonGuide from "../components/workshop/WorkshopCommonGuide";
 
 export default class WorkshopPage extends Component {
 
@@ -7,6 +8,7 @@ export default class WorkshopPage extends Component {
         return(
             <div>
                 <h1>Workshop Page</h1>
+                <WorkshopCommonGuide/>
                 <CreateWorkshop/>
             </div>
         )
