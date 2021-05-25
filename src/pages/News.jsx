8de86bd094842205839b927495d021bcf4dba204
Navinda Lankesh <adilanga@gmail.com>
2021-05-26 00:16:52 +0530

    Initialize News

---
+++ src/pages/News.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+
+export default class News extends Component {
+
+    render() {
+        return(
+            <div>
+                <h1>News</h1>
+            </div>
+        )
+    }
+}
