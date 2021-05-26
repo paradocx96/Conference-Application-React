e60694271526342f827d2148af57a931637f5831
Navinda Lankesh <adilanga@gmail.com>
2021-05-26 15:35:45 +0530

    Homepage Updated

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,13 +1,40 @@
 import React, {Component} from "react";
 import ViewKeyNoteHomepage from "../components/KeyNotes/ViewKeyNoteHomepage";
+import '/src/assets/styles/Homepage.css';
+import {Form, Button, Col, Row, Container} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import FooterBar from "../components/Foobar/FooterBar";
 
 export default class Homepage extends Component {
 
     render() {
-        return(
+        return (
             <div>
-                <h1>Homepage</h1>
-                <ViewKeyNoteHomepage/>
+                <section id="introduction">
+                    <div className="introduction-container">
+                        <h1>RHNA_C0NF3R3NC3 - 2021</h1>
+                        <h3>Sri Lanka Institute of Information Technology</h3>
+                        <a href="#news" className="about-news scrollto">Latest News</a>
+                    </div>
+                </section>
+                <section id="keynotes">
+                    <div className="keynote-container">
+                        <h2>KEY NOTE SPEAKERS</h2>
+                        <br/>
+                        <div>
+                            <ViewKeyNoteHomepage/>
+                        </div>
+                    </div>
+                </section>
+                <section id="news">
+                    <div className="news-container">
+                        <h2>LATEST NEWS</h2>
+                        <br/>
+                        <div>
+
+                        </div>
+                    </div>
+                </section>
             </div>
         )
     }
