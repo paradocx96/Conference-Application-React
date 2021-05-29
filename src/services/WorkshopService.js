84d9bf8d0bb07e8ea2398e0d5c3695760fba55f6
Amarangi Karunathilaka <amarangikarunathilaka@gmail.com>
2021-05-29 21:01:34 +0530

    finish workshop services.

--- src/services/WorkshopService.js
+++ src/services/WorkshopService.js
@@ -3,7 +3,6 @@ import FileDownload from "js-file-download";
 
 const WORKSHOP_API_URL = 'http://localhost:5000/workshop';
 
-
 export function createWorkshop(workshop) {
     console.log(workshop);
     return axios.post(WORKSHOP_API_URL, workshop, {
@@ -15,14 +14,21 @@ export function createWorkshop(workshop) {
         }).catch(error => alert(error));
 }
 
-
 export function getAllWorkshops() {
     return axios.get(WORKSHOP_API_URL);
 }
 
+export function getAllScheduledWorkshop() {
+    return axios.get(WORKSHOP_API_URL + '/scheduled-workshops');
+}
 
-export function downloadDocumentByUserName(formData) {
-    return  axios.post(WORKSHOP_API_URL + '/downloadByUsername',formData,{responseType:'blob'})
+export function getAllPendingWorkshop() {
+    return axios.get(WORKSHOP_API_URL + '/pending-workshops');
+}
+
+export function downloadDocumentByWorkshop(formData) {
+    console.log(formData);
+    return  axios.post(WORKSHOP_API_URL + '/download-documents',formData,{responseType:'blob'})
         .then( (response) => {
             let headline =  response.request.getResponseHeader('Content-Disposition');
             let startFileNameIndex = headline.indexOf('=')+1;
@@ -30,4 +36,26 @@ export function downloadDocumentByUserName(formData) {
             let filename =  headline.substring(startFileNameIndex,endFileNameIndex);
             FileDownload(response.data,filename+".pdf");
         })
+}
+
+export function deleteWorkshop(formData) {
+    return  axios.post(WORKSHOP_API_URL + '/delete',formData,{})
+        .then(response => {
+            if(response.status === 201){
+                alert("Successfully deleted workshop");
+            }
+        }).catch(error => alert(error));
+}
+
+export function approveWorkshop(formData) {
+    return  axios.post(WORKSHOP_API_URL + '/approve',formData,{})
+        .then(response => {
+            if(response.status === 201){
+                alert("Successfully successfully approved the workshop");
+            }
+        }).catch(error => alert(error));
+}
+
+export function updateWorkshop() {
+    return axios.get(WORKSHOP_API_URL + '/update');
 }
