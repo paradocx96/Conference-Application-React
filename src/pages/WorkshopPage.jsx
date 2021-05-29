573d9b8f56e614015889c121e39da95111bf87e5
Amarangi Karunathilaka <amarangikarunathilaka@gmail.com>
2021-05-29 21:10:22 +0530

    Add container to Workshop page.

--- src/pages/WorkshopPage.jsx
+++ src/pages/WorkshopPage.jsx
@@ -1,18 +1,19 @@
 import React, {Component} from "react";
 import CreateWorkshop from "../components/workshop/CreateWorkshop";
 import WorkshopCommonGuide from "../components/workshop/WorkshopCommonGuide";
-import WorkshopList from "../components/workshop/WorkshopList";
+import WorkshopAllList from "../components/workshop/WorkshopAllList";
+import {Container} from "react-bootstrap";
 
 export default class WorkshopPage extends Component {
 
     render() {
         return(
-            <div>
-                <h1>Workshop Page</h1>
+            <Container>
+                <h1 className="mt-5 mb-4">Workshop Page</h1>
                 <WorkshopCommonGuide/>
                 <CreateWorkshop/>
-                <WorkshopList/>
-            </div>
+                <WorkshopAllList/>
+            </Container>
         )
     }
 }
