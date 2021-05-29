cefb52f42896a0e7e616349b76f6ae29418a89f0
Amarangi Karunathilaka <amarangikarunathilaka@gmail.com>
2021-05-29 21:58:16 +0530

    create show pending workshops for reviewer.

---
+++ src/components/workshop/WorkshopAllPendingList.jsx
@@ -0,0 +1,32 @@
+import React, {Component} from "react";
+import WorkshopUnit from "./WorkshopUnit";
+import {Card} from "react-bootstrap";
+import {getAllPendingWorkshop} from "../../services/WorkshopService";
+
+export default class WorkshopAllPendingList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            workshopList: []
+        }
+    };
+
+    renderWorkshopList = () => {
+        return this.state.workshopList.map((item, index) => <WorkshopUnit workshop={item} key={index}/>)
+    }
+
+    componentDidMount() {
+        getAllPendingWorkshop().then(value => this.setState({workshopList: value.data}));
+    }
+
+    render() {
+        return (
+            <Card className="mx-2 my-3 p-2 pt-4">
+                <h2 className="px-2">Scheduled Meetings</h2>
+                {
+                    this.renderWorkshopList()
+                }
+            </Card>
+        )
+    }
+}
