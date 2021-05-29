80b1a15251582e37825d99917700aa290fcc4f59
Amarangi Karunathilaka <amarangikarunathilaka@gmail.com>
2021-05-29 23:40:16 +0530

    commit WorkshopPage.jsx

--- src/pages/WorkshopPage.jsx
+++ src/pages/WorkshopPage.jsx
@@ -3,6 +3,8 @@ import CreateWorkshop from "../components/workshop/CreateWorkshop";
 import WorkshopCommonGuide from "../components/workshop/WorkshopCommonGuide";
 import WorkshopAllList from "../components/workshop/WorkshopAllList";
 import {Container} from "react-bootstrap";
+import WorkshopAllScheduledList from "../components/workshop/WorkshopAllScheduledList";
+import WorkshopAllPendingList from "../components/workshop/WorkshopAllPendingList";
 
 export default class WorkshopPage extends Component {
 
@@ -10,9 +12,21 @@ export default class WorkshopPage extends Component {
         return(
             <Container>
                 <h1 className="mt-5 mb-4">Workshop Page</h1>
+
+                {/**  Only for sign as workshop conductor **/}
                 <WorkshopCommonGuide/>
+
+                {/**  Only for sign as workshop conductor **/}
                 <CreateWorkshop/>
+
+                {/**  Only for sign as admin. - view, download, update/reschedule, delete **/}
                 <WorkshopAllList/>
+
+                {/**  Only for sign as reviewer - view, download, approve **/}
+                <WorkshopAllPendingList/>
+
+                {/**  All signed users - view, download **/}
+                <WorkshopAllScheduledList/>
             </Container>
         )
     }
