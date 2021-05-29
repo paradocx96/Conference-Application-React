c430e75658db3d5624889084d0c56d39b2ec38b9
Amarangi Karunathilaka <amarangikarunathilaka@gmail.com>
2021-05-29 21:43:37 +0530

    create show scheduled workshops for all.

---
+++ src/components/workshop/WorkshopAllScheduledList.jsx
@@ -0,0 +1,32 @@
+import React, {Component} from "react";
+import WorkshopUnit from "./WorkshopUnit";
+import {Card} from "react-bootstrap";
+import {getAllScheduledWorkshop} from "../../services/WorkshopService";
+
+export default class WorkshopAllScheduledList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            workshopList: []
+        }
+    };
+
+    renderWorkshopList = () => {
+        return this.state.workshopList.map((item, index) => <WorkshopUnit workshop={item} key={index}/>)
+    }
+
+    componentDidMount() {
+        getAllScheduledWorkshop().then(value => this.setState({workshopList: value.data}));
+    }
+
+    render() {
+        return (
+            <Card className="mx-2 my-3 p-2 pt-4">
+                <h2 className="px-2">Scheduled Meetings</h2>
+                {
+                    this.renderWorkshopList()
+                }
+            </Card>
+        )
+    }
+}
