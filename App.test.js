7e17c73a641a46f752263c3db8bc12b64bb70bd7
Amarangi Karunathilaka <amarangikarunathilaka@gmail.com>
2021-06-05 08:46:06 +0530

    Write test case to test render of the Conference title.

--- App.test.js
+++ App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import App from './App';
-
+import {shallow} from "enzyme";
 const renderer = require('react-test-renderer');
 
 describe('Conference Application  test cases', () => {
@@ -8,5 +8,11 @@ describe('Conference Application  test cases', () => {
         renderer.create(<App/>);
     });
 
+
+    const title = "ICAF -2021";
+    let wrapped = shallow(<title>{title}</title>);
+    it('should render the Title Component correctly', () => {
+        expect(wrapped).toMatchSnapshot();
+    });
 
 });
