f147679dc2792b964b28ee4af2723d5eb473bf8c
paradocx96 <adilanga@gmail.com>
2021-06-07 11:10:23 +0530

    KeyNotes REST URL Updated!

--- src/services/KeyNoteService.jsx
+++ src/services/KeyNoteService.jsx
@@ -3,37 +3,44 @@
 import axios from "axios";
 
 // TODO: Declare url for rest api
-const KEYNOTE_API_BASE_URL = "http://localhost:8080/keynote/";
-const KEYNOTE_API_BASE_URL_AWS = "http://rhna-env.eba-irftcrbc.us-east-1.elasticbeanstalk.com/keynote";
+const COMMON_URL = "http://localhost:8080";
+const KEYNOTE_API = "/keynote";
+const KEYNOTE_API_BASE_URL = COMMON_URL + KEYNOTE_API;
 
 export default new class KeyNoteService {
 
     // TODO: Method for add new Keynote to database
     postKeyNote(keynote) {
-        return axios.post(KEYNOTE_API_BASE_URL, keynote);
+        return axios.post(KEYNOTE_API_BASE_URL + "/add", keynote);
     }
 
+    // TODO: Method for get all Keynote from database
     getKeyNotes() {
-        return axios.get(KEYNOTE_API_BASE_URL);
+        return axios.get(KEYNOTE_API_BASE_URL + "/get");
     }
 
+    // TODO: Method for get Keynote by Status
     getKeyNotesByStatus(status) {
-        return axios.get(KEYNOTE_API_BASE_URL + "get-by-status/" + status);
+        return axios.get(KEYNOTE_API_BASE_URL + "/get-by-status/" + status);
     }
 
+    // TODO: Method for get Keynote By Id
     getKeyNoteById(id) {
-        return axios.get(KEYNOTE_API_BASE_URL + "get-by-id/" + id);
+        return axios.get(KEYNOTE_API_BASE_URL + "/get-by-id/" + id);
     }
 
+    // TODO: Method for update Keynote's Status
     updateKeyNoteStatus(id, status) {
-        return axios.put(KEYNOTE_API_BASE_URL +"update-status/"+ id, status)
+        return axios.put(KEYNOTE_API_BASE_URL + "/update-status/" + id, status)
     }
 
+    // TODO: Method for Delete a Keynote by ID
     deleteKeyNoteById(id) {
-        return axios.delete(KEYNOTE_API_BASE_URL + id);
+        return axios.delete(KEYNOTE_API_BASE_URL + "/delete/" + id);
     }
 
+    //TODO: Method for update Keynote
     updateKeynote(keynote, id) {
-        return axios.put(KEYNOTE_API_BASE_URL + id, keynote);
+        return axios.put(KEYNOTE_API_BASE_URL + "/update/" + id, keynote);
     }
 }
