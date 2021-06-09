b2d0f31aecce61034c9f1cff1764e41d562fb907
Amarangi Karunathilaka <amarangikarunathilaka@gmail.com>
2021-06-09 14:26:13 +0530

    add auth headers for workshop services methods.

--- src/services/WorkshopService.js
+++ src/services/WorkshopService.js
@@ -1,65 +1,68 @@
 import axios from "axios";
 import FileDownload from "js-file-download";
+import AuthHeader from "./AuthHeader";
 
-const WORKSHOP_API_URL = 'http://localhost:5000/workshop';
+const WORKSHOP_API_URL = 'http://localhost:8080/workshop';
 
 export function createWorkshop(workshop) {
     console.log(workshop);
-    return axios.post(WORKSHOP_API_URL, workshop, {
-    })
+    return axios.post(WORKSHOP_API_URL + '/add', workshop, {headers: AuthHeader()})
         .then(response => {
-            if(response.status === 201){
+            if (response.status === 201) {
                 alert("Successfully created");
             }
         }).catch(error => alert(error));
 }
 
 export function getAllWorkshops() {
-    return axios.get(WORKSHOP_API_URL);
+    return  axios.get(WORKSHOP_API_URL + '/all-workshops', {headers: AuthHeader()});
 }
 
 export function getAllScheduledWorkshop() {
-    return axios.get(WORKSHOP_API_URL + '/scheduled-workshops');
+    return axios.get(WORKSHOP_API_URL + '/scheduled-workshops', {headers: AuthHeader()});
 }
 
 export function getAllPendingWorkshop() {
-    return axios.get(WORKSHOP_API_URL + '/pending-workshops');
+    return axios.get(WORKSHOP_API_URL + '/pending-workshops', {headers: AuthHeader()});
 }
 
 export function downloadDocumentByWorkshop(formData) {
     console.log(formData);
-    return  axios.post(WORKSHOP_API_URL + '/download-documents',formData,{responseType:'blob'})
-        .then( (response) => {
-            let headline =  response.request.getResponseHeader('Content-Disposition');
-            let startFileNameIndex = headline.indexOf('=')+1;
+    return axios.post(WORKSHOP_API_URL + '/download-documents', formData, {
+        headers: AuthHeader(),
+        responseType: 'blob'
+    })
+        .then((response) => {
+            let headline = response.request.getResponseHeader('Content-Disposition');
+            let startFileNameIndex = headline.indexOf('=') + 1;
             let endFileNameIndex = headline.lastIndexOf('"');
-            let filename =  headline.substring(startFileNameIndex,endFileNameIndex);
-            FileDownload(response.data,filename+".pdf");
+            let filename = headline.substring(startFileNameIndex, endFileNameIndex);
+            FileDownload(response.data, filename + ".pdf");
         })
 }
 
 export function deleteWorkshop(formData) {
-    return  axios.post(WORKSHOP_API_URL + '/delete',formData,{})
+    return axios.post(WORKSHOP_API_URL + '/delete', formData, {headers: AuthHeader()})
         .then(response => {
-            if(response.status === 201){
+            if (response.status === 201) {
                 alert("Successfully deleted workshop");
             }
         }).catch(error => alert(error));
 }
 
 export function approveWorkshop(formData) {
-    return  axios.post(WORKSHOP_API_URL + '/approve',formData,{})
+    return axios.post(WORKSHOP_API_URL + '/approve', formData, {headers: AuthHeader()})
         .then(response => {
-            if(response.status === 201){
+            if (response.status === 201) {
                 alert("Successfully approved the workshop");
             }
         }).catch(error => alert(error));
 }
 
 export function updateWorkshop(formData) {
-    return axios.put(WORKSHOP_API_URL + '/update',formData,{})
+    return axios.put(WORKSHOP_API_URL + '/update', formData, {headers: AuthHeader()})
         .then(response => {
-            if(response.status === 201){
+            if (response.status === 201) {
                 alert("Successfully updated the workshop");
             }
         }).catch(error => alert(error));
