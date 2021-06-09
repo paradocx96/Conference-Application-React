d39e7108e641d19d5299be2bef4e722dbbe13db4
Amarangi Karunathilaka <amarangikarunathilaka@gmail.com>
2021-06-09 15:48:52 +0530

    create different view for workshop conductor and other users.

--- src/pages/WorkshopPage.jsx
+++ src/pages/WorkshopPage.jsx
@@ -5,31 +5,41 @@ import WorkshopAllList from "../components/workshop/WorkshopAllList";
 import {Container} from "react-bootstrap";
 import WorkshopAllScheduledList from "../components/workshop/WorkshopAllScheduledList";
 import WorkshopAllPendingList from "../components/workshop/WorkshopAllPendingList";
+import UserService from "../services/UserService";
 
 export default class WorkshopPage extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            signRole: "SIGNED_USER" // ADMIN, REVIEWER, SIGNED_USER, WORKSHOP_CONDUCTOR
+            signRole: "" // ADMIN, REVIEWER, SIGNED_USER, WORKSHOP_CONDUCTOR
         }
-        console.log(this.state)
     }
 
     componentDidMount() {
-        this.setState({signRole: "ADMIN"}); // TODO: SET THE SIGN ROLE HERE.
+        try {
+            const user = UserService.getCurrentUser();
+            if (user) {
+                this.setState({
+                    signRole: user.roles.includes("ROLE_USER_WORKSHOP") ? "ROLE_USER_WORKSHOP" : ""
+                });
+            }
+        } catch (error) {
+
+        }
     }
+
 
     render() {
         return (
             <Container>
-                <h1 style={{webkitTextStroke: '2px black', webkitTextFillColor: 'white',fontWeight: 700}}
+                <h1 style={{webkitTextStroke: '2px black', webkitTextFillColor: 'white', fontWeight: 700}}
                     className="mt-5 mb-4">Workshop Page</h1>
 
                 {
                     /**
                      * Only for sign as workshop conductor
                      */
-                    this.state.signRole === "WORKSHOP_CONDUCTOR" &&
+                    this.state.signRole === "ROLE_USER_WORKSHOP" &&
                     <React.Fragment>
                         <WorkshopCommonGuide/>
                         <CreateWorkshop/>
