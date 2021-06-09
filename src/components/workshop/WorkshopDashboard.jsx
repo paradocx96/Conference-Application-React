650bc4a8ca54664f3873497ebeab83068e98a969
Amarangi Karunathilaka <amarangikarunathilaka@gmail.com>
2021-06-09 14:40:57 +0530

    setup admin view of workshop in dashboard.

--- src/components/workshop/WorkshopDashboard.jsx
+++ src/components/workshop/WorkshopDashboard.jsx
@@ -2,27 +2,20 @@ import React, {Component} from "react";
 import {Link} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import DashboardPanel from "../Dashboard/DashboardPanel";
+import WorkshopAllList from "./WorkshopAllList";
 
 export default class WorkshopDashboard extends Component {
 
     render() {
-        return(
+        return (
             <div>
                 <DashboardPanel/>
-                <section id="subSection">
+                <section id="subSection" className="min-vh-100">
                     <h1>Workshop Dashboard</h1>
-
-
-
 
                     {/*  Do your things here  */}
                     {/*  TODO: Show workshop details here for Admin  */}
-
-
-
-
-
-
+                    <WorkshopAllList/>
                 </section>
             </div>
         )
