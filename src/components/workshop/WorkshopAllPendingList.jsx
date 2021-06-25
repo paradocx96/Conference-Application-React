1635308c3f823e754d0ddb3da337bcfc5ddb6abd
Amarangi Karunathilaka <amarangikarunathilaka@gmail.com>
2021-06-25 10:01:00 +0530

    add alert feedback for workshop approved.

--- src/components/workshop/WorkshopAllPendingList.jsx
+++ src/components/workshop/WorkshopAllPendingList.jsx
@@ -1,7 +1,7 @@
 import React, {Component} from "react";
 import WorkshopUnit from "./WorkshopUnit";
-import {Card,Spinner} from "react-bootstrap";
-import {getAllPendingWorkshop} from "../../services/WorkshopService";
+import {Button, Card, Col, Row, Spinner} from "react-bootstrap";
+import {approveWorkshop, downloadDocumentByWorkshop, getAllPendingWorkshop} from "../../services/WorkshopService";
 
 export default class WorkshopAllPendingList extends Component {
     constructor(props) {
@@ -12,6 +12,20 @@ export default class WorkshopAllPendingList extends Component {
         }
     };
 
+    async handleDownload(workshopId) {
+        const formData = new FormData();
+        formData.append("id", workshopId);
+        await downloadDocumentByWorkshop(formData);
+    }
+
+    async handleApprove(workshopId) {
+        const formData = new FormData();
+        formData.append("id", workshopId);
+        await approveWorkshop(formData);
+        await this.componentDidMount();
+        alert("The workshop has been approved successfully");
+    }
+
     renderWorkshopList = () => {
         if (this.state.isLoading && this.state.workshopList.length === 0) {
             return <div style={{minHeight: '25vh', color: 'gray'}} className="text-center pt-5">
@@ -20,8 +34,29 @@ export default class WorkshopAllPendingList extends Component {
         } else if (this.state.workshopList.length === 0) {
             return <span className="text-danger">There is no any pending workshops for approbation </span>
         } else {
-            return this.state.workshopList.map((item, index) =>
-                <WorkshopUnit workshop={item} key={index} signRole={"REVIEWER"}/>);
+            return this.state.workshopList.map((workshop, index) =>
+                <Card className="mx-2 my-2 px-4 py-3">
+                    <span>{workshop.username}</span><br/>
+                    <span>{workshop.title}</span><br/>
+                    {workshop.courseCode !== '' && <>
+                        <span>{workshop.courseCode}</span><br/></>}
+                    <span>{workshop.venue}</span><br/>
+                    <span>{workshop.date}</span><br/>
+                    <span>{workshop.startingTime}</span><br/>
+                    {workshop.endTime !== '' && <><span>{workshop.endTime}</span><br/></>}
+                    {workshop.description !== '' && <>
+                        <span>{workshop.description}</span><br/></>}
+                    <Row>
+                        <Col xs={6}>
+                            <Button className="px-3 px-sm-5 py-2"
+                                    onClick={() => this.handleDownload(workshop.id)}>Download</Button>
+                        </Col>
+                        <Col xs={6}>
+                            <Button variant="success" className="px-3 px-sm-5 py-2"
+                                    onClick={() => this.handleApprove(workshop.id)}>Approve</Button>
+                        </Col>
+                    </Row>
+                </Card>);
         }
 
     }
