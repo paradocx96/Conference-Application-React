6aad580ef188a22e651485e9cb7befe0a9d89df4
paradocx96 <adilanga@gmail.com>
2021-06-26 23:55:12 +0530

    Initialize Admin Download Edit

--- src/components/Download/EditDownload.jsx
+++ src/components/Download/EditDownload.jsx
@@ -1,2 +1,15 @@
 // TODO: IT19180526 - Chandrasiri S A N L D
 
+import React, {Component} from 'react';
+
+class EditDownload extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default EditDownload;
