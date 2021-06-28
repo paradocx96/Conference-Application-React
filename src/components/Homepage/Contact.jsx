6b35b1f72c80b5d7f7e396fc146169dc533f91f2
paradocx96 <adilanga@gmail.com>
2021-06-28 11:37:30 +0530

    Contact Form Function Done!

--- src/components/Homepage/Contact.jsx
+++ src/components/Homepage/Contact.jsx
@@ -8,13 +8,41 @@ class Contact extends Component {
     // TODO: Initializing state values and functions
     constructor(props) {
         super(props);
-        this.state = {
-            name: '',
-            email: '',
-            subject: '',
-            message: ''
-        }
-        this.submit = this.submit.bind(this);
+        this.state = this.initialState;
+        this.onSubmit = this.onSubmit.bind(this);
+        this.onReset = this.onReset.bind(this);
+        this.onChange = this.onChange.bind(this);
+    }
+
+    // TODO: Initializing default values
+    initialState = {
+        name: '',
+        email: '',
+        subject: '',
+        message: ''
+    }
+
+    // TODO: Assign form values to State variables
+    onChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    // TODO: Submit form values
+    onSubmit = (event) => {
+        event.preventDefault();
+        console.log('Email Send!');
+        console.log('Name : ', this.state.name);
+        console.log('Email : ', this.state.email);
+        console.log('Subject : ', this.state.subject);
+        console.log('Message : ', this.state.message);
+        this.onReset();
+    }
+
+    // TODO: Reset form values
+    onReset = () => {
+        this.setState(() => this.initialState)
     }
 
     render() {
@@ -25,23 +53,40 @@ class Contact extends Component {
                     <Row>
                         <Col>1 of 2</Col>
                         <Col>
-                            <Form>
-                                <Form.Group controlId="Name">
-                                    <Form.Control placeholder="Your Name" />
-                                </Form.Group>
-                                <Form.Group controlId="Email">
-                                    <Form.Control type="email" placeholder="Your Email" />
-                                </Form.Group>
-                                <Form.Group controlId="Subject">
-                                    <Form.Control placeholder="Subject" />
-                                </Form.Group>
-                                <Form.Group controlId="Message">
-                                    <Form.Control as="textarea" placeholder="Message" rows={5} />
-                                </Form.Group>
-                                <Button variant="primary" type="submit">
-                                    Send Message
-                                </Button>
-                            </Form>
+                            <div className="pt-5 pb-5">
+                                <Form onSubmit={this.onSubmit.bind(this)}>
+                                    <Form.Group controlId="Name">
+                                        <Form.Control placeholder="Your Name"
+                                                      name="name"
+                                                      value={this.state.name}
+                                                      onChange={this.onChange}/>
+                                    </Form.Group>
+                                    <Form.Group controlId="Email">
+                                        <Form.Control type="email"
+                                                      placeholder="Your Email"
+                                                      name="email"
+                                                      value={this.state.email}
+                                                      onChange={this.onChange}/>
+                                    </Form.Group>
+                                    <Form.Group controlId="Subject">
+                                        <Form.Control placeholder="Subject"
+                                                      name="subject"
+                                                      value={this.state.subject}
+                                                      onChange={this.onChange}/>
+                                    </Form.Group>
+                                    <Form.Group controlId="Message">
+                                        <Form.Control as="textarea"
+                                                      placeholder="Message"
+                                                      rows={5}
+                                                      name="message"
+                                                      value={this.state.message}
+                                                      onChange={this.onChange}/>
+                                    </Form.Group>
+                                    <Button variant="primary" type="submit">
+                                        Send Message
+                                    </Button>
+                                </Form>
+                            </div>
                         </Col>
                     </Row>
                 </Container>
