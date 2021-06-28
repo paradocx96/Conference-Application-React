c86289018c66164151a8e3bfb0ea57a10b11f5a4
paradocx96 <adilanga@gmail.com>
2021-06-28 16:53:15 +0530

    ImportantDates Updated

--- src/components/Homepage/ImportantDates.jsx
+++ src/components/Homepage/ImportantDates.jsx
@@ -2,7 +2,7 @@
 
 import React, {Component} from "react";
 
-import {Button} from 'react-bootstrap';
+import {Card, Col, Container, Row} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default class ImportantDates extends Component {
@@ -14,7 +14,88 @@ export default class ImportantDates extends Component {
     render() {
         return (
             <div>
-                <h1>Sample Data</h1>
+                <Container>
+                    <Row>
+                        <Col>
+                            <Card bg={'secondary'} text={'white'}>
+                                <Card.Body>
+                                    <Card.Text><b>01st MAY 2021</b></Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col>
+                            <Card bg={'dark'} text={'white'} border={'primary'}>
+                                <Card.Body>
+                                    <Card.Text><b>REGISTRATION (OPEN)</b></Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    </Row>
+                    <Row className="pt-4">
+                        <Col>
+                            <Card bg={'dark'} text={'white'} border={'primary'}>
+                                <Card.Body>
+                                    <Card.Text><b>PAPER SUBMISSION (OPEN)</b></Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col>
+                            <Card bg={'secondary'} text={'white'}>
+                                <Card.Body>
+                                    <Card.Text><b>05th MAY 2021</b></Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    </Row>
+                    <Row className="pt-4">
+                        <Col>
+                            <Card bg={'secondary'} text={'white'}>
+                                <Card.Body>
+                                    <Card.Text><b>5th JULY 2021</b></Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col>
+                            <Card bg={'dark'} text={'white'} border={'primary'}>
+                                <Card.Body>
+                                    <Card.Text><b>PAPER SUBMISSION (CLOSE)</b></Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    </Row>
+                    <Row className="pt-4">
+                        <Col>
+                            <Card bg={'dark'} text={'white'} border={'primary'}>
+                                <Card.Body>
+                                    <Card.Text><b>REGISTRATION (CLOSE)</b></Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col>
+                            <Card bg={'secondary'} text={'white'}>
+                                <Card.Body>
+                                    <Card.Text><b>10th JULY 2021</b></Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    </Row>
+                    <Row className="pt-4">
+                        <Col>
+                            <Card bg={'secondary'} text={'white'}>
+                                <Card.Body>
+                                    <Card.Text><b>15th, 16th JULY 2021</b></Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col>
+                            <Card bg={'dark'} text={'white'} border={'primary'}>
+                                <Card.Body>
+                                    <Card.Text><b>CONFERENCE</b></Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    </Row>
+                </Container>
             </div>
         )
     }
