c0345f742cc35e60d551ea3feaf8c1704e1b76fd
paradocx96 <adilanga@gmail.com>
2021-06-28 16:13:08 +0530

    ConferenceTracks Updated!

--- src/components/Homepage/Gallery.jsx
+++ src/components/Homepage/Gallery.jsx
@@ -4,6 +4,15 @@ import React, {Component} from "react";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Image} from 'react-bootstrap';
+
+// TODO: Import Images
+// import Image1 from '../../assets/images/Gallery/Image1.jpg';
+// import Image2 from '../../assets/images/Gallery/Image2.jpg';
+// import Image3 from '../../assets/images/Gallery/Image3.jpg';
+// import Image4 from '../../assets/images/Gallery/Image4.jpg';
+// import Image5 from '../../assets/images/Gallery/Image5.jpg';
+
+// TODO: Parcel Image Transform
 import Image1 from 'url:../../assets/images/Gallery/Image1.jpg?as=webp&width=300&height=250';
 import Image2 from 'url:../../assets/images/Gallery/Image2.jpg?as=webp&width=300&height=250';
 import Image3 from 'url:../../assets/images/Gallery/Image3.jpg?as=webp&width=300&height=250';
@@ -14,6 +23,11 @@ export default class Gallery extends Component {
 
     constructor(props) {
         super(props);
+    }
+
+    imageSize = {
+        width: 300,
+        height: 250
     }
 
     render() {
--- src/components/Homepage/Organise.jsx
+++ src/components/Homepage/Organise.jsx
@@ -3,7 +3,12 @@
 import React, {Component} from "react";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Image} from 'react-bootstrap';
+import {Card, Col, Container, Image, Row} from 'react-bootstrap';
+
+// TODO: Import Images
+// import SLIIT from './../../assets/images/Sponsored/sliit.png';
+
+// TODO: Parcel Image Transform
 import SLIIT from 'url:../../assets/images/Sponsored/sliit.png?as=webp';
 
 export default class Organise extends Component {
@@ -12,10 +17,41 @@ export default class Organise extends Component {
         super(props);
     }
 
+    imageSize = {
+        width: 300,
+        height: 250
+    }
+
     render() {
         return (
             <div>
-                <Image src={SLIIT} rounded/>
+                <Container>
+                    <Row>
+                        <Col>
+
+                        </Col>
+
+                        <Col>
+
+                        </Col>
+
+                        <Col>
+                            <Card>
+                                <Card.Body>
+                                    <Card.Img variant="top" src={SLIIT}/>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+
+                        <Col>
+
+                        </Col>
+
+                        <Col>
+
+                        </Col>
+                    </Row>
+                </Container>
             </div>
         )
     }
